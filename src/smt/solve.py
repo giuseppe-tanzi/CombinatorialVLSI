40d11315a1cfe3bd2b36f964358a5916738963b3
Giuseppe Tanzi <giuseppetan@hotmail.it>
2022-07-06 15:35:05 +0200

    Added rotation

--- src/smt/solve.py
+++ src/smt/solve.py
@@ -34,21 +34,23 @@ class SMTsolver:
         _, self.max_width, self.circuits = instance
         self.circuits_num = len(self.circuits)
 
-        self.w = IntVector('widths', self.circuits_num)
-        self.h = IntVector('heights', self.circuits_num)
+        widths, heights = ([i for i, _ in self.circuits], [j for _, j in self.circuits])
 
-        for i in range(self.circuits_num):
-            self.w[i], _ = self.circuits[i]
-            _, self.h[i] = self.circuits[i]
+        if not self.rotation:
+            self.w = IntVector('widths', self.circuits_num)
+            self.h = IntVector('heights', self.circuits_num)
+            for i in range(self.circuits_num):
+                self.w[i], _ = self.circuits[i]
+                _, self.h[i] = self.circuits[i]
 
-        lower_bound = sum([self.h[i] * self.w[i] for i in range(self.circuits_num)]) // self.max_width
-        upper_bound = sum(self.h) - min(self.h)
+        lower_bound = sum([heights[i] * widths[i] for i in range(self.circuits_num)]) // self.max_width
+        upper_bound = sum(heights) - min(heights)
 
         for plate_height in range(lower_bound, upper_bound + 1):
             self.sol = Solver()
             self.sol.set(timeout=self.timeout * 1000)
 
-            self.set_constraints(plate_height)
+            self.set_constraints(plate_height, widths, heights)
 
             solve_time = time.time()
             if self.sol.check() == sat:
@@ -60,37 +62,39 @@ class SMTsolver:
             else:
                 try_timeout = round((self.timeout - (time.time() - solve_time)))
                 if try_timeout <= 0:
+                    print(f'{ins_num})', (None, 0), try_timeout)
                     return None, 0
         return None, 0
 
-    def set_constraints(self, plate_height):
+    def set_constraints(self, plate_height, widths, heights):
 
         self.x_positions = IntVector('x_pos', self.circuits_num)
         self.y_positions = IntVector('y_pos', self.circuits_num)
 
-        # actual widths and heights if rotation allowed
-        w, h = self.w, self.h
-
         # Handling rotation
         if self.rotation:
+            self.w = IntVector('widths', self.circuits_num)
+            self.h = IntVector('heights', self.circuits_num)
+            rotations = IntVector('rotations', self.circuits_num)
+            self.sol.add([And(0 <= rotations[i], rotations[i] <= 1) for i in range(self.circuits_num)])
+            self.sol.add([Implies(widths[i] == heights[i], rotations[i] == 0) for i in range(self.circuits_num)])
             for i in range(self.circuits_num):
-                # if rotation allowed and a circuit is square  (width=height) then force it to be not rotated
-                self.sol.add(If(w[i] == h[i], And(w[i] == self.w[i], h[i] == self.h[i]),
-                                Or(And(w[i] == self.w[i], h[i] == self.h[i]),
-                                   And(w[i] == self.h[i], h[i] == self.w[i]))))
+                self.sol.add(If(rotations[i] == 1, And(widths[i] == self.h[i], heights[i] == self.w[i]),
+                                And(widths[i] == self.w[i], heights[i] == self.h[i])))
 
         # CONSTRAINTS
 
-        # NOT EXCEED, DOMAINS
+        # Domains
         self.sol.add([And(0 <= self.x_positions[i], self.x_positions[i] <= self.max_width - self.w[i])
                       for i in range(self.circuits_num)])
 
         self.sol.add([And(0 <= self.y_positions[i], self.y_positions[i] <= plate_height - self.h[i])
                       for i in range(self.circuits_num)])
 
-        # DO NOT OVERLAP
+
         for i in range(1, self.circuits_num):
             for j in range(0, i):
+                # Don't overlap
                 self.sol.add(Or(Sum(self.y_positions[i], self.h[i]) <= self.y_positions[j],
                                 Sum(self.y_positions[j], self.h[j]) <= self.y_positions[i],
                                 Sum(self.x_positions[i], self.w[i]) <= self.x_positions[j],
@@ -113,17 +117,23 @@ class SMTsolver:
                                         Sum(self.x_positions[j],
                                             self.w[j]) <= self.x_positions[i])))
 
-        # SUM OVER ROWS (CUMULATIVE)
+        # Cumulative over rows
         for u in range(plate_height):
             self.sol.add(
                 self.max_width >= Sum([If(And(self.y_positions[i] <= u, u < Sum(self.y_positions[i], self.h[i])),
                                           self.w[i], 0) for i in range(self.circuits_num)]))
 
-        # SUM OVER COLUMNS (CUMULATIVE)
+        # Cumulative over columns
         for u in range(self.max_width):
             self.sol.add(plate_height >= Sum([If(And(self.x_positions[i] <= u, u < Sum(self.x_positions[i], self.w[i])),
                                                  self.h[i], 0) for i in range(self.circuits_num)]))
 
     def evaluate(self):
-        return [(self.w[i], self.h[i], int(self.sol.model().evaluate(self.x_positions[i]).as_string()),
+        if self.rotation:
+            widths = [int(self.sol.model().evaluate(self.w[i]).as_string()) for i in range(self.circuits_num)]
+            heights = [int(self.sol.model().evaluate(self.h[i]).as_string()) for i in range(self.circuits_num)]
+        else:
+            widths, heights = self.w, self.h
+
+        return [(widths[i], heights[i], int(self.sol.model().evaluate(self.x_positions[i]).as_string()),
                  int(self.sol.model().evaluate(self.y_positions[i]).as_string())) for i in range(self.circuits_num)]
