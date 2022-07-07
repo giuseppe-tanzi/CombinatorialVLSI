b44a375dd31773e8f118cc589036d8452280d067
Giuseppe Tanzi <giuseppetan@hotmail.it>
2022-07-07 09:44:43 +0200

    Improved the model without rotations

--- src/smt/solve.py
+++ src/smt/solve.py
@@ -1,7 +1,8 @@
 import time
 
+import numpy as np
 from utils.utils import write_solution
-from z3 import And, Or, sat, Solver, Sum, If, IntVector, Implies
+from z3 import And, Or, sat, Sum, If, IntVector, Implies, Tactic
 
 
 class SMTsolver:
@@ -33,12 +34,12 @@ class SMTsolver:
     def solve_instance(self, instance, ins_num):
         _, self.max_width, self.circuits = instance
         self.circuits_num = len(self.circuits)
+        self.w = IntVector('widths', self.circuits_num)
+        self.h = IntVector('heights', self.circuits_num)
 
         widths, heights = ([i for i, _ in self.circuits], [j for _, j in self.circuits])
 
         if not self.rotation:
-            self.w = IntVector('widths', self.circuits_num)
-            self.h = IntVector('heights', self.circuits_num)
             for i in range(self.circuits_num):
                 self.w[i], _ = self.circuits[i]
                 _, self.h[i] = self.circuits[i]
@@ -47,9 +48,11 @@ class SMTsolver:
         upper_bound = sum(heights) - min(heights)
 
         for plate_height in range(lower_bound, upper_bound + 1):
-            self.sol = Solver()
+            self.sol = Tactic('qflra').solver()
+            # self.sol = Tactic('qflia').solver()
+            # z3.describe_tactics()
             self.sol.set(timeout=self.timeout * 1000)
-            self.sol.set(threads=8)
+            # self.sol.set(threads=4)
 
             self.set_constraints(plate_height, widths, heights)
 
@@ -72,10 +75,14 @@ class SMTsolver:
         self.x_positions = IntVector('x_pos', self.circuits_num)
         self.y_positions = IntVector('y_pos', self.circuits_num)
 
+        areas_index = np.argsort([heights[i] * widths[i] for i in range(self.circuits_num)])
+        areas_index = areas_index[::-1]
+        self.w = [self.w[areas_index[i]] for i in range(self.circuits_num)]
+        self.h = [self.h[areas_index[i]] for i in range(self.circuits_num)]
+        # biggests = areas_index[-1], areas_index[-2]
+
         # Handling rotation
         if self.rotation:
-            self.w = IntVector('widths', self.circuits_num)
-            self.h = IntVector('heights', self.circuits_num)
             rotations = IntVector('rotations', self.circuits_num)
             self.sol.add([And(0 <= rotations[i], rotations[i] <= 1) for i in range(self.circuits_num)])
             self.sol.add([Implies(widths[i] == heights[i], rotations[i] == 0) for i in range(self.circuits_num)])
@@ -100,33 +107,38 @@ class SMTsolver:
                                 Sum(self.x_positions[i], self.w[i]) <= self.x_positions[j],
                                 Sum(self.x_positions[j], self.w[j]) <= self.x_positions[i]))
 
+                # # symmetry breaking : fix relative position of the two biggest rectangles
+                # self.sol.add(Or(self.x_positions[biggests[1]] > self.x_positions[biggests[0]],
+                #                 And(self.x_positions[biggests[1]] == self.x_positions[biggests[0]],
+                #                     self.y_positions[biggests[1]] >= self.y_positions[biggests[0]])))
+
                 # Two rectangles with same dimensions
                 self.sol.add(Implies(And(self.w[i] == self.w[j], self.h[i] == self.h[j]),
                                      Or(self.x_positions[j] > self.x_positions[i],
                                         And(self.x_positions[j] == self.x_positions[i],
                                             self.y_positions[j] >= self.y_positions[i]))))
-
+                #
                 # If two rectangles cannot be packed side to side along the x axis
-                self.sol.add(Implies(Sum(self.w[i], self.w[j]) > self.max_width,
-                                     Or(Sum(self.y_positions[i], self.h[i]) <= self.y_positions[j],
-                                        Sum(self.y_positions[j], self.h[j]) <= self.y_positions[i])))
+                # self.sol.add(Implies(Sum(self.w[i], self.w[j]) > self.max_width,
+                #                      Or(Sum(self.y_positions[i], self.h[i]) <= self.y_positions[j],
+                #                         Sum(self.y_positions[j], self.h[j]) <= self.y_positions[i])))
+                #
+                # # If two rectangles cannot be packed one over the other along the y axis
+                # self.sol.add(Implies(Sum(self.h[i], self.h[j]) > plate_height,
+                #                      Or(Sum(self.x_positions[i], self.w[i]) <= self.x_positions[j],
+                #                         Sum(self.x_positions[j],
+                #                             self.w[j]) <= self.x_positions[i])))
 
-                # If two rectangles cannot be packed one over the other along the y axis
-                self.sol.add(Implies(Sum(self.h[i], self.h[j]) > plate_height,
-                                     Or(Sum(self.x_positions[i], self.w[i]) <= self.x_positions[j],
-                                        Sum(self.x_positions[j],
-                                            self.w[j]) <= self.x_positions[i])))
-
-        # Cumulative over rows
-        for u in range(plate_height):
-            self.sol.add(
-                self.max_width >= Sum([If(And(self.y_positions[i] <= u, u < Sum(self.y_positions[i], self.h[i])),
-                                          self.w[i], 0) for i in range(self.circuits_num)]))
-
-        # Cumulative over columns
-        for u in range(self.max_width):
-            self.sol.add(plate_height >= Sum([If(And(self.x_positions[i] <= u, u < Sum(self.x_positions[i], self.w[i])),
-                                                 self.h[i], 0) for i in range(self.circuits_num)]))
+        # # Cumulative over rows
+        # for u in range(plate_height):
+        #     self.sol.add(
+        #         self.max_width >= Sum([If(And(self.y_positions[i] <= u, u < Sum(self.y_positions[i], self.h[i])),
+        #                                   self.w[i], 0) for i in range(self.circuits_num)]))
+        #
+        # # Cumulative over columns
+        # for u in range(self.max_width):
+        #     self.sol.add(plate_height >= Sum([If(And(self.x_positions[i] <= u, u < Sum(self.x_positions[i], self.w[i])),
+        #                                          self.h[i], 0) for i in range(self.circuits_num)]))
 
     def evaluate(self):
         if self.rotation:
