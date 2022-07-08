6188d06057ebf0f8f7d056edb62b13329f6a4685
Flavio <fla.pinzarrone@stud.uniroma3.it>
2022-07-08 20:08:00 +0200

    removed implied constraints

--- src/lp/solve_rot.py
+++ src/lp/solve_rot.py
@@ -1,28 +1,15 @@
 import time
-
 import numpy as np
 from ortools.linear_solver import pywraplp
 from utils.utils import write_solution
 
+from src.lp.solve import LPsolver
 
-class LPsolver:
+
+class LPsolverRot(LPsolver):
 
     def __init__(self, data, output_dir, timeout):
-        self.data = data
-        if output_dir == "":
-            output_dir = "../data/output_lp/"
-        self.output_dir = output_dir
-        self.timeout = timeout
-        self.ins_num = None
-
-    def solve(self):
-        solutions = []
-        for d in self.data:
-            self.ins_num = d[0]
-            solution = self.solve_instance(d)
-            solutions.append(solution)
-            # write_solution(ins_num, solution[0], solution[1])
-        return solutions
+        super().__init__(data, output_dir, timeout)
 
     def solve_instance(self, instance):
         start = time.time()
@@ -47,11 +34,21 @@ class LPsolver:
 
             x = []
             y = []
+            widths = []
+            heights = []
 
             # widths_r[i] = widths[i] - rot[i]*widths[i] + rot[i]*heights[i]
             for i in range(self.circuits_num):
-                x.append(solver.IntVar(lb=0, ub=self.max_width - w[i], name=f'x_{i}'))
-                y.append(solver.IntVar(lb=0, ub=max_h - h[i], name=f'y_{i}'))
+                x.append(solver.IntVar(lb=0, ub=max_x, name=f'x_{i}'))
+                y.append(solver.IntVar(lb=0, ub=max_y, name=f'y_{i}'))
+                widths.append(solver.IntVar(lb=min(w), ub=max(w), name=f'widths_{i}'))
+                heights.append(solver.IntVar(lb=min(h), ub=max(h), name=f'heights_{i}'))
+
+            rot = []
+            for i in range(self.circuits_num):
+                rot.append(solver.IntVar(lb=0, ub=1, name=f'rot_{i}'))
+                solver.Add(widths[i] == w[i] - rot[i] * w[i] + rot[i] * h[i])
+                solver.Add(heights[i] == h[i] - rot[i] * h[i] + rot[i] * w[i])
 
             # disjunction bool variables
             d = [
@@ -59,16 +56,16 @@ class LPsolver:
                 for i in range(self.circuits_num)]
             M = 1000
             for i in range(self.circuits_num):
-                # # domain constraints
-                # solver.Add(x[i] + w[i] <= self.max_width)
-                # solver.Add(y[i] + h[i] <= max_h)
+                # domain constraints
+                solver.Add(x[i] + widths[i] <= self.max_width)
+                solver.Add(y[i] + heights[i] <= max_h)
                 # non overlapping constraints
                 for j in range(i + 1, self.circuits_num):
                     solver.Add(sum(d[i][j]) >= 1)
-                    solver.Add(x[i] + w[i] <= x[j] + M * (1 - d[i][j][0]))
-                    solver.Add(x[j] + w[j] <= x[i] + M * (1 - d[i][j][1]))
-                    solver.Add(y[i] + h[i] <= y[j] + M * (1 - d[i][j][2]))
-                    solver.Add(y[j] + h[j] <= y[i] + M * (1 - d[i][j][3]))
+                    solver.Add(x[i] + widths[i] <= x[j] + M * (1 - d[i][j][0]))
+                    solver.Add(x[j] + widths[j] <= x[i] + M * (1 - d[i][j][1]))
+                    solver.Add(y[i] + heights[i] <= y[j] + M * (1 - d[i][j][2]))
+                    solver.Add(y[j] + heights[j] <= y[i] + M * (1 - d[i][j][3]))
 
             # # cumulative constraint over rows
             # c_w = [[solver.IntVar(lb=0, ub=max(w), name=f'cw_{i}_{u}') for u in range(max_h)] for i in
@@ -106,8 +103,10 @@ class LPsolver:
             # print('Total time elapsed: ', total_time)
 
             if status == pywraplp.Solver.OPTIMAL:
-                circuit_pos = [(width, height, x, y) for width, height, x, y in
-                               zip(w, h, [a.solution_value() for a in x], [a.solution_value() for a in y])]
+                # print([r.solution_value() for r in rot])
+                circuit_pos = [(w, h, x, y) for w, h, x, y in
+                               zip([a.solution_value() for a in widths], [a.solution_value() for a in heights],
+                                   [a.solution_value() for a in x], [a.solution_value() for a in y])]
 
                 # self.print_solution(C, X, max_h)
                 write_solution(self.output_dir, self.ins_num, ((self.max_width, max_h), circuit_pos), total_time)
